0a855aaf3e499b1ae48a1dbd5e05d79a1acbd105
Den Hafiz <den.m.hafiz@gmail.com>
2023-03-24 11:50:14 +0800

    Change socket and app route function

--- app.py
+++ app.py
@@ -5,17 +5,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8f2009a07dab679c5d31366451a955263246e5e8cf17d12ff8fbc142072feab4#'
 socketio = SocketIO(app)
 
+@socketio.on('message')
+def handle_message(message):
+    print("Received message: " + message)
+    if message != "User connected!":
+        send(message, broadcast=True)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-def messageReceived(methods=['GET', 'POST']):
-    print('Message was received!')
-
-@socketio.on('my event')
-def handle_my_event(json, methods=['GET', 'POST']):
-    print('Received my event: ' + str(json))
-    socketio.emit('My response', json, callback=messageReceived)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
