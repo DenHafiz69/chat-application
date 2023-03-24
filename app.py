5c55a657683bae90a9bb38ff465d824ad0d5280e
Den Hafiz <den.m.hafiz@gmail.com>
2023-03-24 11:51:58 +0800

    Import send

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8f2009a07dab679c5d31366451a955263246e5e8cf17d12ff8fbc142072feab4#'
