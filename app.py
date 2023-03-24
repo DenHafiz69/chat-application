cb574b574127585850b8a1d81350285e4d5e68d1
Den Hafiz <den.m.hafiz@gmail.com>
2023-03-24 11:17:36 +0800

    Rewrite from scratch

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = '8f2009a07dab679c5d31366451a955263246e5e8cf17d12ff8fbc142072feab4#'
 socketio = SocketIO(app)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
