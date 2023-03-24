59c53913754130f7a1dee5587e038040e0f1ba83
Den Hafiz <den.m.hafiz@gmail.com>
2023-03-24 10:32:59 +0800

    Initialization

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-# Import Flask and render_template module. render_template is used to render the webpages
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
-# Import socketIO and send module from flask_socketIO
-from flask_socketio import SocketIO, send
-
-# Creates a new Flask app instance
 app = Flask(__name__)
-app.config['SECRET'] = "secret!123" # Sets a secret key, this is used to keep the client sessions secure
-socketio = SocketIO(app, cors_allowed_origins="*")
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
-@socketio.on('message')
-def handle_message(message):
-    print("Received message: " + message)
-    if message != "User connected!":
-        send(message, broadcast=True)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
 
 if __name__ == "__main__":
-    socketio.run(app, host="localhost")
+    socketio.run(app)
