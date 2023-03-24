c38ad772e24b6cece3daa872d338adb2fc196323
Den Hafiz <den.m.hafiz@gmail.com>
2023-03-24 14:36:44 +0800

    Test for socket code in script.js

--- templates/script.js
+++ templates/script.js
@@ -11,3 +11,20 @@ input.addEventListener("keypress", function(event) {
         document.getElementById("sendBtn").click();
     }
 });
+
+$(document).ready(function() {
+    var socket = io.connect("http://localhost:5000")
+    socket.on('connect', function() {
+        socket.send("User connected!");
+    });
+
+    socket.on('message', function(data) {
+        $('#messages').append($('<p>').text(data));
+    });
+
+    $('#sendBtn').on('click', function() {
+        socket.send($('#username').val() + ': ' + $('#message').val());
+        $('#message').val('');
+    })
+
+});
