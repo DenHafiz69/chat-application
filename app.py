6e5614b9735f098fe57c2f4896e3615701ce9099
Den Hafiz <den.m.hafiz@gmail.com>
2023-03-24 14:46:29 +0800

    Update from main

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+# Import Flask and render_template module. render_template is used to render the webpages
 from flask import Flask, render_template
+
+# Import socketIO and send module from flask_socketIO
 from flask_socketio import SocketIO, send
 
+# Creates a new Flask app instance
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8f2009a07dab679c5d31366451a955263246e5e8cf17d12ff8fbc142072feab4#'
-socketio = SocketIO(app)
+app.config['SECRET'] = "secret!123" # Sets a secret key, this is used to keep the client sessions secure
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on('message')
 def handle_message(message):
@@ -15,5 +19,5 @@ def handle_message(message):
 def index():
     return render_template("index.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     socketio.run(app, debug=True)
